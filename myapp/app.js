b8f1ad2debc2563b3a906e4d6db31cd18c311173
Gab625 <bielprado93@gmail.com>
2025-11-25 16:45:40 -0300

    feat aula05

--- myapp/app.js
+++ myapp/app.js
@@ -1,23 +1,35 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/",(req, res) => {
-    res.send("Hello World");
-})
+const path = require("path");
+app.use("/public", express.static("static"));
 
-app.post("/", (req, res)=>{
-    res.send("Got a POST request at /user")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.put("/user", (req, res)=>{
-    res.send("Got a PUT request at /user")
-})
+app.post("/", (req, res) => {
+  res.send("Got a POST request at /user");
+});
 
-app.delete("/user", (req, res)=>{
-    res.send("Got a DELETE request at /user")
-})
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
+
+//tratamento
+app.use((req, res, next) => {
+  res.status(404); // mensagem na web
+  res.sendFile(path.join(__dirname, "error", "404page.html"));
+  console.log("Rota não encontrada"); // mensagem na linha de comando
+});
+
+// res.status(404).sendFile(path.join(__dirname, "error", "404page.html"));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`);
+});
