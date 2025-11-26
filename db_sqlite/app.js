258a9deb6c1d19961f9a3950688d1087246fb6d8
Antonio <arpy.adm@gmail.com>
2025-11-25 21:27:13 -0300

    feat AULA6_AULA7

---
+++ db_sqlite/app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const sqlite3 = require('sqlite3')
+const fs = require('fs')
+const cheerio = require('cheerio')
+const { json } = require('stream/consumers')
+const { error } = require('console')
+
+const db = new sqlite3.Database('./bd.sqlite', (err) => {
+    if (err) console.error(err.message)
+
+    console.log('Conectado ao banco de dados')
+})
+
+app.get('/pedidos', (req, res) => {
+    const sql = 'SELECT * FROM pedido'
+
+    // Execução da instrução SQL no BD
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message })
+            return
+        }
+
+        const htmlBase = fs.readFileSync('./views/pedidos.html')
+        const cheerioLoad = cheerio.load(htmlBase)
+        const tabelaCorpo = cheerioLoad('#tabela-corpo')
+        
+        rows.forEach(pedido => {
+tabelaCorpo.append(`
+<tr>
+<th>${pedido.NUM_PED}</th>
+<td>${pedido.PRAZO_ENTR}</td>
+<td>${pedido.CD_CLI}</td>
+<td>${pedido.CD_VEND}</td>
+</tr>`)
+})
+res.send(cheerioLoad.html())
+})
+})
+
+app.get('/pedido/:id', (req, res, next)=> {
+    const sql = "SELECT * FROM ITEM_PEDIDO as t1 LEFT JOIN PRODUTO as t2 ON t1.CD_PROD = t2.COD_PROD WHERE NO_PED = ?"
+
+    db.all(sql,[req.params.id], (err, rows) => {
+        if(err){
+            res.status(500).json({error:err.message})
+            return
+        }
+
+        const htmlBase = fs.readFileSync('./views/pedido.html')
+        const cheerioLoad = cheerio.load(htmlBase)
+        const tabelaCorpo = cheerioLoad('#items-pedido')
+        tabelaCorpo.empty()
+
+        rows.forEach(pedido => {
+tabelaCorpo.append(`
+<div class="row g-0 linha">
+                    <div class="col-1 text-center">${pedido.CD_PROD}</div>
+                    <div class="col-4">${pedido.DESC_PROD}</div>
+                    <div class="col-1 text-center">${pedido.UNID_PROD}</div>
+                    <div class="col-2 text-center">${pedido.QTD_PED}</div>
+                    <div class="col-2 text-right">${pedido.VAL_UNIT}</div>
+                    <div class="col-2 text-right">${pedido.VAL_UNIT * pedido.QTD_PED}</div>
+                </div>`)
+})
+
+
+
+
+
+
+
+res.send(cheerioLoad.html())
+
+    })
+})
+// Servidor
+app.listen(port, () => {
+    console.log(`Servidor ouvindo a porta ${port}!`)
+})
+
+
+
